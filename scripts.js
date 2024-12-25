e6ece16b0017a2b9431dcec1a82cd1b2c84b78a1
Alex Pace <pacealexwork@gmail.com>
2024-12-25 20:25:30 +0100

    form check y logo añadido

---
+++ scripts.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+    initializeListeners()
+})
+
+const contactForm = document.getElementById("contact-form")
+const contactName = document.getElementById("contact-name")
+const contactEmail = document.getElementById("contact-email")
+const contactMessage = document.getElementById("contact-message")
+const contactSubmit = document.getElementById("contact-submit")
+const submitValidationMessage = document.getElementById("submit-validation-message")
+
+function initializeListeners() {
+    contactForm.addEventListener("submit", submitContactForm)
+    contactName.addEventListener("input", validateContact)
+    contactEmail.addEventListener("input", validateContact)
+    contactMessage.addEventListener("input", validateContact)
+}
+
+function submitContactForm(event) {
+    event.preventDefault()
+
+    if (validateContact()) {
+        console.log("Form submitted")
+        showToast()
+        clearContactForm()
+    }
+}
+
+function validateContact() {
+    const isValid = contactForm.checkValidity()
+
+    if (isValid) {
+        contactSubmit.disabled = false
+        submitValidationMessage.textContent = " El formulario se puede enviar"
+
+    } else contactSubmit.disabled = true
+
+    return isValid
+}
+
+function clearContactForm() {
+    contactName.value = ""
+    contactEmail.value = ""
+    contactMessage.value = ""
+    submitValidationMessage.textContent = " El formulario debe haber sido rellenado correctamente para poder enviarse"
+    contactSubmit.disabled = true
+}
+
+function showToast() {
+    const toast = document.getElementById('toast')
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
+
+    toastBootstrap.show()
+}
